2a98c099e74f5c9a05df5dca9bb6309491822d5a
Adwait Pande <adwait.pande11@gmail.com>
2020-04-20 16:06:46 +0530

    Added code to create DB objects in dev environment if not exist

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,19 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+import os
 
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+# Constants
+LOCAL_SQLITE_FILENAME = 'db.sqlite3'
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + LOCAL_SQLITE_FILENAME
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+if os.environ.get('FLASK_ENV') == 'development' and not os.path.isfile(LOCAL_SQLITE_FILENAME):
+    db.create_all()
+
 bcrypt = Bcrypt(app)
